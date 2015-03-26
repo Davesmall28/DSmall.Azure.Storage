61ae5956d90627ac23a1d65b7fa1847881508ab9
Davesmall28 <dave@dsmall.co.uk>
2015-03-26 09:31:44 +0000

    Published wrong version to NuGet.
    Incremented assembly version and republished to NuGet.

--- DSmall.Azure.Storage/DSmall.Azure.Storage/NuGetPush.bat
+++ DSmall.Azure.Storage/DSmall.Azure.Storage/NuGetPush.bat
@@ -1,3 +1,5 @@
 ..\NuGet\NuGet pack DSmall.Azure.Storage.csproj -Build -symbols
 
+..\NuGet\NuGet push DSmall.Azure.Storage.1.0.1.1.nupkg
+
 pause
--- DSmall.Azure.Storage/DSmall.Azure.Storage/Properties/AssemblyInfo.cs
+++ DSmall.Azure.Storage/DSmall.Azure.Storage/Properties/AssemblyInfo.cs
@@ -11,5 +11,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("2bbaf2c7-e45b-47d8-b820-b1ca9cb783e5")]
-[assembly: AssemblyVersion("1.0.1.0")]
-[assembly: AssemblyFileVersion("1.0.1.0")]
+[assembly: AssemblyVersion("1.0.1.1")]
+[assembly: AssemblyFileVersion("1.0.1.1")]
