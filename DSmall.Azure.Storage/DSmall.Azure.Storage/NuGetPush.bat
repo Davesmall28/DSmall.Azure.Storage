fff510c5de7ce8f2c1e8b028cea4a419c3f3394f
Davesmall28 <dave@dsmall.co.uk>
2015-03-26 09:34:24 +0000

    New wrapper classes were missed from project file on check in.

--- DSmall.Azure.Storage/DSmall.Azure.Storage/NuGetPush.bat
+++ DSmall.Azure.Storage/DSmall.Azure.Storage/NuGetPush.bat
@@ -1,5 +1,5 @@
 ..\NuGet\NuGet pack DSmall.Azure.Storage.csproj -Build -symbols
 
-..\NuGet\NuGet push DSmall.Azure.Storage.1.0.1.1.nupkg
+..\NuGet\NuGet push DSmall.Azure.Storage.1.0.2.0.nupkg
 
 pause
